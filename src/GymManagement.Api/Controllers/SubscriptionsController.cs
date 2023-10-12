1c6699f9c9662f3b9fce891705648daefde921ee
Rory Cawley <rory.cawley@gmail.com>
2023-10-12 12:20:00 +0100

    add subscription service to container

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,3 +1,4 @@
+using GymManagement.Application.Services;
 using GymManagement.Contracts.Subscriptions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +8,18 @@ namespace GymManagement.Api.Controllers;
 [Route("[controller]")]
 public class SubscriptionsController : ControllerBase
 {
+    private readonly ISubscriptionsService _subscriptionsService;
+
+    public SubscriptionsController(ISubscriptionsService subscriptionsService)
+    {
+        _subscriptionsService = subscriptionsService;
+    }
+
     [HttpPost]
     public IActionResult CreateSubscription(CreateSubscriptionRequest request)
     {
-        return Ok(request);
+        var subscriptionId = _subscriptionsService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
+        var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
+        return Ok(response); // 201 Created
     }
 }
--- src/GymManagement.Api/Program.cs
+++ src/GymManagement.Api/Program.cs
@@ -1,3 +1,5 @@
+using GymManagement.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ISubscriptionsService, SubscriptionsService>();
 
 var app = builder.Build();
 
